b90feca6e7439e9701a577b8005d9d449a7c16dd
bringforthjoy101 <adelugba.emma@gmail.com>
2020-11-05 13:09:05 +0000

    Initial Upload

--- routes/routes.js
+++ routes/routes.js
@@ -32,7 +32,7 @@ var upload = multer({
         cb(null, {fieldName: file.fieldname});
       },
       key: function (req, file, cb) {
-        cb(null, Date.now().toString()`_${file.originalname}`)
+        cb(null, Date.now().toString()+'_'+file.originalname)
       }
     })
   })
