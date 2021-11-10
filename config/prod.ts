6250c1681dfa747ab9c7ca053aafcc1552dc3a0e
Emmanuel Adelugba <adelugba.emma@gmail.com>
2021-11-10 21:28:00 +0100

    ...

--- config/prod.ts
+++ config/prod.ts
@@ -1,33 +1,33 @@
 let prod = {} as any
 // psql -h 18.116.173.28 -d fpinvoiceapp -U postgres -W
-prod.DBNAME = "fountainpng_invoicer"
-prod.DBUSERNAME="fountainpng_invoicer"
-prod.DBPASSWORD="P@ssword123"
-prod.DBDIALECT = "mysql"
-prod.DBPORT = 3306
-prod.DBREGION = 'us-east-1'
-prod.DBHOST = '216.194.161.130' //18.116.173.28
+prod.DBNAME = process.env.DBNAME
+prod.DBUSERNAME= process.env.DBUSERNAME
+prod.DBPASSWORD= process.env.DBPASSWORD
+prod.DBDIALECT = process.env.DBDIALECT
+prod.DBPORT = process.env.DBPORT
+prod.DBREGION = process.env.DBREGION
+prod.DBHOST = process.env.DBHOST //18.116.173.28
 prod.PORT = process.env.PORT || 1122
-prod.JWTSECRET = 'S74R73RK17'
-prod.JWT_EXPIRY_TIME = '1h'
-prod.SSL = true
-prod.BUCKETNAME = 'bringforthjoy' //'whizpoolblogtemp'
-prod.REGION = 'us-east-1'
-prod.ACL = 'public-read-write' // 'public-read'
-prod.ACCESSKEY = 'AKIAJKPAWJAYBJFL224A' 
-prod.SECRETKEY = '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'
+prod.JWTSECRET = process.env.JWTSECRET
+prod.JWT_EXPIRY_TIME = process.env.JWT_EXPIRY_TIME
+prod.SSL = process.env.SSL
+prod.BUCKETNAME = process.env.BUCKETNAME //'whizpoolblogtemp'
+prod.REGION = process.env.REGION
+prod.ACL = process.env.ACL // 'public-read'
+prod.ACCESSKEY = process.env.ACCESSKEY
+prod.SECRETKEY = process.env.SECRETKEY
 
 // EMAIL CREDENTIALS
-prod.MAIL_HOST = 'smtp.gmail.com'
-prod.MAIL_SECURE = 'ssl'
-prod.MAIL_USERNAME = 'adelugba.emma@gmail.com'
-prod.MAIL_PASSWORD = 'temitopeemmanuel'
-prod.MAIL_FROM = 'adelugba.emma@gmail.com'
+prod.MAIL_HOST = process.env.MAIL_HOST
+prod.MAIL_SECURE = process.env.MAIL_SECURE
+prod.MAIL_USERNAME = process.env.MAIL_USERNAME
+prod.MAIL_PASSWORD = process.env.MAIL_PASSWORD
+prod.MAIL_FROM = process.env.MAIL_FROM
 
 prod.ROUTES_EXCLUDED_FROM_AUTH = ["/test",'','/','/auth','/invoices/uploadFileToS3','/getDownloadUrl','/fetchSignature', '/login', '/register']
 
 // Response Messages
-prod.VALIDATE_TYPE_OF_DATA_RESP_MSG = "{{RESOURCE}} '{{DATA}}' must be a {{TYPE}}!"
+prod.VALIDATE_TYPE_OF_DATA_RESP_MSG = process.env.VALIDATE_TYPE_OF_DATA_RESP_MSG
 
 
 export default prod
